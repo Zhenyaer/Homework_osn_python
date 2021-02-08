c820574f58a09fc739aa43e5531a23afce62e8cc
zhenyaer <zhenyaer@mail.ru>
2021-02-08 13:15:26 +0400

    lesson6

---
+++ lesson6_task2.py
@@ -0,0 +1,16 @@
+class Road:
+    def __init__(self, length, width):
+        # длина и ширина дорожного полотна (м)
+        self._length = length
+        self._width = width
+
+    def massa(self):
+        # удельная масса (кг/(м2*см)) и толщина (см)
+        m = 25
+        width = 10
+        mass = self._length * self._width * m * width / 1000
+        print(f'Для строительства дороги потребуется {int(mass)} тонн асфальта.')
+
+
+road_mass = Road(10000, 25)
+road_mass.massa()
